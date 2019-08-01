4cf505d955faf3429d63f30a7d03e8c5bac5a1d1
devil-geek <devil.prowler@gmail.com>
2019-07-31 23:06:55 -0500

    Changed project card

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -8,8 +8,8 @@ const ProjectCard = props => {
   }
   return (
     <div key={project.node.id} className="row">
-      <div className="col-8-10 col-centered">
-        <div
+      <div className="col-10-10 col-centered project-card">
+        {/* <div
           className="project-card"
           style={{
             backgroundImage:
@@ -18,6 +18,7 @@ const ProjectCard = props => {
               ')',
           }}
         >
+
           <div className="project-card-content">
             <div className="project-card-content__meta">
               {project.node.tech ? (
@@ -49,6 +50,55 @@ const ProjectCard = props => {
               <Button buttonText="Visit the Website" />
             </a>
           </div>
+        </div> */}
+        <div className="project-card">
+          <section>
+            <div className="frame">
+              <div
+                className="img"
+                style={{
+                  backgroundImage:
+                    'url(' +
+                    project.node.thumbnailImage.childImageSharp.fluid.src +
+                    ')',
+                }}
+              />
+              <div className="border">
+                <div className="project-card-content">
+                  <h1 className="project-card-content__title">
+                    {project.node.title}
+                  </h1>
+                  <h2>
+                    {project.node.company}
+                  </h2>
+                  <div className="project-card-content__meta">
+                    {project.node.tech ? (
+                      <p>
+                        {project.node.tech.map((tag, index) => (
+                          <span className="tag" key={tag}>
+                            {tag}
+                            {index === project.node.tech.length - 1
+                              ? ''
+                              : ' | '}{' '}
+                          </span>
+                        ))}
+                      </p>
+                    ) : null}
+                  </div>
+                  <p className="project-card-content__excerpt">
+                    {project.node.description}
+                  </p>
+                  <a
+                    href={project.node.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button buttonText="Visit the Website" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </div>
